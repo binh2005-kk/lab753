1ed8b94a813425d0cf39afdbc416ee12e3250bf2
binh <binlun411@gmail.com>
2025-04-22 09:09:51 +0700

    update

--- src/main/java/com/cmucs426/model/mama.java
+++ src/main/java/com/cmucs426/model/mama.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package hihi;
+package com.cmucs426.model;
 
 /**
  *
  * @author ACER
  */
-public class ahaha {
+public class mama {
     
 }
